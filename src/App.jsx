e5e2bad5e3a91879abcd5dc4e794e4af3b8e7095
varunkasyap <77223408+varunkasyap@users.noreply.github.com>
2024-11-03 13:59:53 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Fragment, useState } from "react"
+import Header from "./components/Header.jsx"
+import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
+
+function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const inputIsValid = userInput.duration >= 1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue
+      };
+    }
+    );
+  }
+
+  return (
+    <Fragment>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {!inputIsValid && <p className="center">please enter duration greater than 0</p>}
+      {inputIsValid && <Results input={userInput} />}
+    </Fragment>
+  )
+}
+
+export default App
